fd1ca5bfb6fddf5a0cafef0a029614cee2ecb097
ErwanDecoster <wanerd2@gmail.com>
2025-04-22 16:29:09 +0200

    feat: init

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "@fontsource-variable/anybody/wdth.css";
+import "@fontsource-variable/mulish/index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <div></div>
+  </StrictMode>
+);
