e5640d2636110d1a31fa83deee6cbce5fc4249fa
ErwanDecoster <wanerd2@gmail.com>
2025-04-22 16:46:34 +0200

    fix: update JSX syntax for consistency and readability in __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,11 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <div className="md:px-10 lg:px-20 mx-auto w-full px-6">
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </div>
   ),
-})
+});
