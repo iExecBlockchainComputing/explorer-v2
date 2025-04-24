409de64c0edddbb2ab6d8fb5e4c8aacb504566a9
ErwanDecoster <wanerd2@gmail.com>
2025-04-24 11:38:10 +0200

    feat: add prettier configuration and format code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import "@fontsource-variable/anybody/wdth.css";
-import "@fontsource-variable/mulish/index.css";
+import '@fontsource-variable/anybody/wdth.css';
+import '@fontsource-variable/mulish/index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div></div>
   </StrictMode>
