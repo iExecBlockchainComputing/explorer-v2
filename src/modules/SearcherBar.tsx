347109d6927b67cc243c33a09073af235a5cc909
ErwanDecoster <wanerd2@gmail.com>
2025-05-01 16:52:16 +0200

    fix: adjust SearcherBar styling for improved layout and responsiveness

--- src/modules/SearcherBar.tsx
+++ src/modules/SearcherBar.tsx
@@ -7,14 +7,17 @@ export function SearcherBar({ className }: { className?: string }) {
   return (
     <div className={cn('relative m-auto w-full max-w-2xl', className)}>
       <Input
-        className="bg-input border-secondary w-full rounded-2xl py-6 pr-32 pl-8"
+        className="bg-input border-secondary w-full rounded-2xl py-5.5 pr-32 pl-12 sm:py-6.5"
         placeholder="Search address or id  or txHash"
       />
-      <Search size="18" className="absolute top-1/2 left-2 -translate-y-1/2" />
+      <Search
+        size="18"
+        className="absolute top-1/2 left-4 -translate-y-1/2 sm:left-6"
+      />
       <Button
         size="sm"
         variant="outline"
-        className="bg-input hover:bg-secondary absolute top-1/2 right-2 -translate-y-1/2"
+        className="bg-input hover:bg-secondary absolute top-1/2 right-4 -translate-y-1/2 sm:right-6"
       >
         My activity
       </Button>
