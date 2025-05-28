c09e2e68253a8cd001123d002ed323da81d33f88
ErwanDecoster <wanerd2@gmail.com>
2025-05-28 18:43:32 +0200

    fix: update redirect target to use INITIAL_CHAIN slug

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,8 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
+import { INITIAL_CHAIN } from '@/utils/chain.utils';
 
 export const Route = createFileRoute('/')({
   loader: () => {
-    return redirect({ to: '/bellecour' });
+    return redirect({ to: INITIAL_CHAIN.slug });
   },
 });
