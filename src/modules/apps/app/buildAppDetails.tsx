50d88465fe851866c31e2b941af72599574508f9
ErwanDecoster <wanerd2@gmail.com>
2025-06-03 19:52:53 +0200

    refactor: remove debug log from buildAppDetails function

--- src/modules/apps/app/buildAppDetails.tsx
+++ src/modules/apps/app/buildAppDetails.tsx
@@ -11,7 +11,6 @@ import {
 export function buildAppDetails({ app }: { app: App }) {
   const firstTransfer = Array.isArray(app?.transfers) && app?.transfers[0];
   const firstTimestamp = firstTransfer?.transaction?.timestamp;
-  console.log('app', app);
 
   return {
     ...(app.address && {
