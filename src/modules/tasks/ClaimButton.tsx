fdc976ac95acac0691e5faf7aa46d24b870aa375
ErwanDecoster <wanerd2@gmail.com>
2025-06-09 14:09:32 +0200

    fix: correct deadline and add loading indicator

--- src/modules/tasks/ClaimButton.tsx
+++ src/modules/tasks/ClaimButton.tsx
@@ -1,4 +1,6 @@
 import { Task, TaskQuery } from '@/graphql/graphql';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { LoaderCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { getIExec } from '@/externals/iexecSdkClient';
 import useUserStore from '@/stores/useUser.store';
@@ -6,7 +8,7 @@ import { pluralize } from '@/utils/pluralize';
 
 function isClaimable(task: Task): boolean {
   const isStatusValid = task.status === 'ACTIVE' || task.status === 'REVEALING';
-  const isBeforeFinalDeadline = task.contributionDeadline * 1000 < Date.now();
+  const isBeforeFinalDeadline = task.finalDeadline * 1000 < Date.now();
   return isStatusValid && isBeforeFinalDeadline;
 }
 
@@ -18,32 +20,37 @@ export function ClaimButton({
   className?: string;
 }) {
   const { isConnected } = useUserStore();
-  if (!isConnected || !tasks) {
-    return;
-  }
-  const claimableTasks = tasks.filter(isClaimable);
+  const queryClient = useQueryClient();
 
-  if (claimableTasks.length === 0) {
-    return null;
-  }
+  const { mutate: claimTasks, isPending } = useMutation({
+    mutationFn: async () => {
+      const iexec = await getIExec();
 
-  const handleClaimAll = async () => {
-    const iexec = await getIExec();
-
-    for (const task of claimableTasks) {
-      try {
-        console.log('Claiming task', task.taskid);
-        const txHash = await iexec.task.claim(task.taskid);
-        console.log(`Task ${task.taskid} claimed with txHash:`, txHash);
-      } catch (error) {
-        console.error(`Failed to claim task ${task.taskid}:`, error);
+      for (const task of claimableTasks) {
+        try {
+          console.log('Claiming task', task.taskid);
+          const txHash = await iexec.task.claim(task.taskid);
+          console.log(`Task ${task.taskid} claimed with txHash:`, txHash);
+        } catch (error) {
+          console.error(`Failed to claim task ${task.taskid}:`, error);
+        }
       }
-    }
-  };
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['task'] });
+      queryClient.invalidateQueries({ queryKey: ['deal', 'tasks'] });
+    },
+  });
+
+  if (!isConnected || !tasks) return null;
+
+  const claimableTasks = tasks.filter(isClaimable);
+  if (claimableTasks.length === 0) return null;
 
   return (
-    <Button variant="link" className={className} onClick={handleClaimAll}>
-      Claim {claimableTasks.length} {pluralize(claimableTasks.length, 'task')}
+    <Button variant="link" className={className} onClick={() => claimTasks()}>
+      Claim {pluralize(claimableTasks.length, 'task')}
+      {isPending && <LoaderCircle className="animate-spin" />}
     </Button>
   );
 }
