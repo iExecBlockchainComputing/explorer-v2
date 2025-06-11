a00c580d3f16b4b1d2ce4dabc127ffb2918767b7
Erwan Decoster <36567542+ErwanDecoster@users.noreply.github.com>
2025-06-11 15:52:22 +0200

    fix: replace substr by substring
    Co-authored-by: pjt <26487010+PierreJeanjacquot@users.noreply.github.com>

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,9 +1,9 @@
 import { multiaddr } from '@multiformats/multiaddr';
 
 export const multiaddrHexToHuman = (hexString: string): string => {
-  if (hexString.substr(0, 2) !== '0x') return hexString;
+  if (hexString.substring(0, 2) !== '0x') return hexString;
   let res: string;
-  const buffer: Buffer = Buffer.from(hexString.substr(2), 'hex');
+  const buffer: Buffer = Buffer.from(hexString.substring(2), 'hex');
   try {
     res = multiaddr(new Uint8Array(buffer)).toString();
   } catch {
