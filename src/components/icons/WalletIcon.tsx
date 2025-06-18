ec601e8e6931e0f631771cc168a808d6b1e547d2
ErwanDecoster <wanerd2@gmail.com>
2025-06-18 16:06:01 +0200

    fix: update SVG attributes to camelCase in IexecAccountIcon and WalletIcon components

--- src/components/icons/WalletIcon.tsx
+++ src/components/icons/WalletIcon.tsx
@@ -10,22 +10,22 @@ const WalletIcon = ({ size = 20, className = '' }) => (
     <path
       d="M18.9617 5.46094H5.03832C3.9126 5.46094 3 6.37354 3 7.49926V16.4993C3 17.625 3.9126 18.5376 5.03832 18.5376H18.9617C20.0874 18.5376 21 17.625 21 16.4993V7.49926C21 6.37354 20.0874 5.46094 18.9617 5.46094Z"
       stroke="#FCD15A"
-      stroke-width="1.56"
-      stroke-linecap="round"
+      strokeWidth="1.56"
+      strokeLinecap="round"
       stroke-linejoin="round"
     />
     <path
       d="M21 9.96019C21 8.83447 20.0874 7.92188 18.9617 7.92188H5.03832C3.9126 7.92188 3 8.83447 3 9.96019"
       stroke="#FCD15A"
-      stroke-width="1.56"
-      stroke-linecap="round"
+      strokeWidth="1.56"
+      strokeLinecap="round"
       stroke-linejoin="round"
     />
     <path
       d="M21 12.5383C21 11.4126 20.0874 10.5 18.9617 10.5H15.4116C15.1471 10.5 14.8924 10.6001 14.6986 10.7802L12.713 12.6257C12.311 12.9992 11.689 12.9992 11.287 12.6257L9.30144 10.7802C9.10764 10.6001 8.853 10.5 8.5884 10.5H5.03832C3.9126 10.5 3 11.4126 3 12.5383"
       stroke="#FCD15A"
-      stroke-width="1.56"
-      stroke-linecap="round"
+      strokeWidth="1.56"
+      strokeLinecap="round"
       stroke-linejoin="round"
     />
   </svg>
