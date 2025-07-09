a928743d3a003852437b759790b12b3d8531596f
ErwanDecoster <wanerd2@gmail.com>
2025-07-09 11:52:14 +0200

    fix: a <button> cannot be nested inside another <button>

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -43,24 +43,26 @@ const CopyButton = ({
   return (
     <TooltipProvider delayDuration={0}>
       <Tooltip open={showTooltip}>
-        <TooltipTrigger className="box-content flex max-w-full items-center gap-1">
-          {displayText && (
-            <span className="overflow-hidden overflow-ellipsis">
-              {displayText}
-            </span>
-          )}
-          <button
-            onClick={(e) => {
-              e.stopPropagation();
-              e.preventDefault();
-              handleCopy();
-            }}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            className="hover:before:bg-grey-700 active:before:bg-grey-600 relative z-0 -mx-1 -my-1 items-center gap-1 px-1 py-1 transition-colors before:absolute before:inset-0 before:-z-10 before:rounded-lg before:duration-150 active:before:scale-x-[0.98] active:before:scale-y-[0.94]"
-          >
-            <Copy className="size-4 flex-none" />
-          </button>
+        <TooltipTrigger asChild>
+          <span className="box-content flex max-w-full items-center gap-1">
+            {displayText && (
+              <span className="overflow-hidden overflow-ellipsis">
+                {displayText}
+              </span>
+            )}
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                handleCopy();
+              }}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              className="hover:before:bg-grey-700 active:before:bg-grey-600 relative z-0 -mx-1 -my-1 items-center gap-1 px-1 py-1 transition-colors before:absolute before:inset-0 before:-z-10 before:rounded-lg before:duration-150 active:before:scale-x-[0.98] active:before:scale-y-[0.94]"
+            >
+              <Copy className="size-4 flex-none" />
+            </button>
+          </span>
         </TooltipTrigger>
         <TooltipContent side="top" className="max-w-sm">
           {tooltipMessage}
