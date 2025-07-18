65b05ee193c37c40bf6bcf231596f7c796ea199d
ErwanDecoster <wanerd2@gmail.com>
2025-07-18 18:20:19 +0200

    refactor: replace SUPPORTED_CHAIN_IDS with a local variable for improved clarity in UnsupportedChain component

--- src/components/UnsupportedChain.tsx
+++ src/components/UnsupportedChain.tsx
@@ -3,12 +3,11 @@ import { useAccount } from 'wagmi';
 import { getSupportedChains } from '@/utils/chain.utils';
 import { Alert, AlertDescription, AlertTitle } from './ui/alert';
 
-const SUPPORTED_CHAIN_IDS = getSupportedChains().map((chain) => chain.id);
-
 export function UnsupportedChain() {
   const { isConnected, chainId } = useAccount();
+  const supportedChainIds = getSupportedChains().map((chain) => chain.id);
   const isChainSupported =
-    chainId !== undefined && SUPPORTED_CHAIN_IDS.includes(chainId);
+    chainId !== undefined && supportedChainIds.includes(chainId);
 
   if (!isConnected || isChainSupported) {
     return null;
