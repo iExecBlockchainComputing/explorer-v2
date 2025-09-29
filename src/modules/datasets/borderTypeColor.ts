7d03e2e220c3c0a0d8f8223e462a8917864f04b4
ErwanDecoster <wanerd2@gmail.com>
2025-09-29 10:35:38 +0200

    feat: update borderTypeColor to include background colors and dark mode support

--- src/modules/datasets/borderTypeColor.ts
+++ src/modules/datasets/borderTypeColor.ts
@@ -1,12 +1,52 @@
 export const borderTypeColor = [
-  { keywords: ['string'], color: 'border-yellow-200 text-yellow-200' },
-  { keywords: ['video'], color: 'border-orange-300 text-orange-300' },
-  { keywords: ['bool'], color: 'border-blue-200 text-blue-200' },
-  { keywords: ['application'], color: 'border-blue-400 text-blue-400' },
-  { keywords: ['audio'], color: 'border-[#A0B1FE] text-[#A0B1FE]' },
-  { keywords: ['f64'], color: 'border-green-200 text-green-200' },
-  { keywords: ['i128'], color: 'border-purple-200 text-purple-200' },
-  { keywords: ['image'], color: 'border-[#F05FC5] text-[#F05FC5]' },
-  { keywords: ['number'], color: 'border-[#F693B8] text-[#F693B8]' },
-  { keywords: ['boolean'], color: 'border-purple-100 text-purple-100' },
+  {
+    keywords: ['string'],
+    color:
+      'bg-yellow-200/30 border-yellow-200 dark:text-yellow-200 dark:bg-transparent',
+  },
+  {
+    keywords: ['video'],
+    color:
+      'bg-orange-300/30 border-orange-300 dark:text-orange-300 dark:bg-transparent',
+  },
+  {
+    keywords: ['bool'],
+    color:
+      'bg-blue-200/30 border-blue-200 dark:text-blue-200 dark:bg-transparent',
+  },
+  {
+    keywords: ['application'],
+    color:
+      'bg-blue-400/30 border-blue-400 dark:text-blue-400 dark:bg-transparent',
+  },
+  {
+    keywords: ['audio'],
+    color:
+      'bg-[#A0B1FE]/30 border-[#A0B1FE] dark:text-[#A0B1FE] dark:bg-transparent',
+  },
+  {
+    keywords: ['f64'],
+    color:
+      'bg-green-200/30 border-green-200 dark:text-green-200 dark:bg-transparent',
+  },
+  {
+    keywords: ['i128'],
+    color:
+      'bg-purple-200/30 border-purple-200 dark:text-purple-200 dark:bg-transparent',
+  },
+  {
+    keywords: ['image'],
+    color:
+      'bg-[#F05FC5]/30 border-[#F05FC5] dark:text-[#F05FC5] dark:bg-transparent',
+  },
+  {
+    keywords: ['number'],
+    color:
+      'bg-[#F693B8]/30 border-[#F693B8] dark:text-[#F693B8] dark:bg-transparent',
+  },
+  {
+    keywords: ['boolean'],
+    color:
+      'bg-purple-100/30 border-purple-100 dark:text-purple-100 dark:bg-transparent',
+  },
 ];
