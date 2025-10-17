d35aa68c08ff6bcbefabf8eeb2ebeb1ede64edcd
ErwanDecoster <wanerd2@gmail.com>
2025-10-17 14:36:10 +0200

    feat: enhance pagination logic to display all pages for 7 or fewer total pages

--- src/components/PaginatedNavigation.tsx
+++ src/components/PaginatedNavigation.tsx
@@ -22,21 +22,36 @@ export const PaginatedNavigation = ({
   const generatePages = () => {
     const pages: (number | 'ellipsis')[] = [];
 
-    if (totalPages <= 5) {
+    if (totalPages <= 7) {
+      // Show all pages if 7 or fewer
       for (let i = 1; i <= totalPages; i++) {
         pages.push(i);
       }
-    } else if (currentPage <= 3) {
-      for (let i = 1; i <= Math.min(currentPage + 2, totalPages); i++) {
-        pages.push(i);
-      }
     } else {
+      // Always show first page
       pages.push(1);
-      pages.push('ellipsis');
 
-      const maxPage = Math.min(currentPage + 2, totalPages);
-      for (let i = currentPage; i <= maxPage; i++) {
-        pages.push(i);
+      if (currentPage <= 3) {
+        // Near beginning: 1 2 3 4 ... last
+        for (let i = 2; i <= 4; i++) {
+          pages.push(i);
+        }
+        pages.push('ellipsis');
+        pages.push(totalPages);
+      } else if (currentPage >= totalPages - 2) {
+        // Near end: 1 ... (last-3) (last-2) (last-1) last
+        pages.push('ellipsis');
+        for (let i = totalPages - 3; i <= totalPages; i++) {
+          pages.push(i);
+        }
+      } else {
+        // In middle: 1 ... (current-1) current (current+1) ... last
+        pages.push('ellipsis');
+        for (let i = currentPage - 1; i <= currentPage + 1; i++) {
+          pages.push(i);
+        }
+        pages.push('ellipsis');
+        pages.push(totalPages);
       }
     }
 
