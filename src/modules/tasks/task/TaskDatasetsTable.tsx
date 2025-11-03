ac2fd19776adddd819fa7dd2a9861a5b836c3583
ErwanDecoster <wanerd2@gmail.com>
2025-11-03 11:10:39 +0100

    feat: add dataset type in task dataset tab

--- src/modules/tasks/task/TaskDatasetsTable.tsx
+++ src/modules/tasks/task/TaskDatasetsTable.tsx
@@ -1,12 +1,49 @@
 import { DETAIL_TABLE_LENGTH, TABLE_REFETCH_INTERVAL } from '@/config';
+import { SchemaSearchPaginatedQuery } from '@/graphql/dataprotector/graphql';
+import { DatasetsQuery } from '@/graphql/poco/graphql';
 import { execute } from '@/graphql/pocoBulk/execute';
 import { useQuery } from '@tanstack/react-query';
 import { DataTable } from '@/components/DataTable';
 import { ErrorAlert } from '@/modules/ErrorAlert';
 import { columns } from '@/modules/datasets/datasetsTable/columns';
+import { useDatasetsSchemas } from '@/modules/datasets/hooks/useDatasetsSchemas';
+import { SchemaFilter } from '@/modules/datasets/schemaFilters';
 import useUserStore from '@/stores/useUser.store';
 import { createPlaceholderDataFnForQueryKey } from '@/utils/createPlaceholderDataFnForQueryKey';
 import { taskDatasetsQuery } from './taskDatasetsPocoBulkQuery';
+
+function formatDataset({
+  dataset,
+  schema,
+  isSchemasLoading,
+}: {
+  dataset:
+    | DatasetsQuery['datasets'][number]
+    | SchemaSearchPaginatedQuery['protectedDatas'][number];
+  schema?: SchemaFilter[];
+  isSchemasLoading: boolean;
+}) {
+  return {
+    address: dataset.address ?? '',
+    name: dataset.name ?? '',
+    schema: schema ?? [],
+    isSchemasLoading: isSchemasLoading,
+    owner: { address: dataset.owner?.address ?? '' },
+    timestamp: dataset.timestamp,
+    transfers:
+      dataset.transfers ??
+      ('transactionHash' in dataset && dataset.transactionHash
+        ? [
+            {
+              transaction: {
+                txHash: dataset.transactionHash,
+              },
+            },
+          ]
+        : []),
+    destination: `/dataset/${dataset.address}`,
+  };
+}
 
 function useTaskDatasetsData({ taskId }: { taskId: string }) {
   const { chainId } = useUserStore();
@@ -25,14 +62,22 @@ function useTaskDatasetsData({ taskId }: { taskId: string }) {
     }
   );
 
-  const formattedDataset =
-    data?.task?.bulkSlice?.datasets.map((dataset) => ({
-      ...dataset,
-      destination: `/dataset/${dataset.address}`,
-    })) ?? [];
+  const datasets = data?.task?.bulkSlice?.datasets || [];
+  const datasetAddresses = datasets.map((dataset) => dataset.address);
+  const { schemasMap, isLoading: isSchemasLoading } = useDatasetsSchemas(
+    datasetAddresses,
+    chainId!
+  );
+  const formattedDatasets = datasets.map((dataset) =>
+    formatDataset({
+      dataset,
+      schema: schemasMap.get(dataset.address) || [],
+      isSchemasLoading,
+    })
+  );
 
   return {
-    data: formattedDataset,
+    data: formattedDatasets,
     isLoading,
     isRefetching,
     isError,
